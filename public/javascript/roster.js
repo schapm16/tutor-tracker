647716576d9cbab9aa5b2ab0067dd383b44b92c9
Stephen Chapman <Schapm16@gmail.com>
2018-03-25 13:20:21 -0400

    Render actual student roster data on Roster.html

--- public/javascript/roster.js
+++ public/javascript/roster.js
@@ -1,9 +1,46 @@
-$(document).ready(function() {
-console.log('Script Running');
+function requestRoster() {
+	return $.get('/file/readRoster');
+}
 
-$.get('/file/readRoster')
-	.done(function(data) {
+function renderRoster(roster) {
+
+for (let i = 1; i < roster.length; i ++) {
+	$('#roster').append(`<div class="col-6">
+		<div class="card bg-light">
+		<div class="card-header"><h3>Student: ${roster[i].name}</h3></div>
+		<div class="card-body">
+			<form>
+				<div class="form-group row">
+					<label for="name" class="col-3">Name</label>
+					<input id="name" class="col-8 form-control" type="text" value="${roster[i].name}">
+				</div>
+
+				<div class="form-group row">
+					<label for="class-code" class="col-3">Class Code</label>
+					<input id="class-code" class="col-8 form-control" type="text" value="${roster[i].classCode}">
+				</div>
+
+				<div class="form-group row">
+					<label for="email" class="col-3">Email</label>
+					<input id="email" class="col-8 form-control" type="text" value="${roster[i].email}">
+				</div>
+			</form>
+		</div>
+		</div>
+		</div>
+		`);
+}
+
+
+}
+
+$(document).ready(function() {
+console.log('Roster script running');
+	
+	requestRoster().done(function(data) {
 		console.log(data);
+		renderRoster(data);
 	});
+
 
 });
